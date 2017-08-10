97973dd032eb8ac39cb7bb976ffc2e2794766ad7
Dnpypy <ball1981@inbox.ru>
2017-08-10 15:49:23 +0300

    Add files via upload

---
+++ lesson1/weight.rb
@@ -0,0 +1,14 @@
+puts "Как Вас зовут?"
+name = gets.chomp
+
+puts "Какой Ваш рост?"
+height = gets.chomp
+
+
+
+weight = height.to_i - 110
+if weight < 0 
+	puts "Ваш вес уже оптимальный"
+else
+	puts "Ваше имя #{name} и Ваш идеальный вес: " + weight.to_s
+end

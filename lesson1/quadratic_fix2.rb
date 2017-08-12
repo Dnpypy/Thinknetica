c801f3b9126d38a32532f391f72e8e5fcc510765
Dnpypy <ball1981@inbox.ru>
2017-08-12 14:46:24 +0300

    quadratic_fix2.rb

--- lesson1/quadratic_fix2.rb
+++ lesson1/quadratic_fix2.rb
@@ -9,11 +9,12 @@ c = gets.chomp.to_f
 
 d = b ** 2 - 4 * a * c
 
-root = Math.sqrt(d)
-
 if d < 0
   puts "корней нет"
-else d < 0
+end
+
+if d > 0
+  root = Math.sqrt(d)
   x1 = -b + root / 2 * a
   x2 = -b - root / 2 * a
 puts "Дискриминант #{d} , 1 корень #{x1.to_f}, 2 корень #{x2.to_f}"
